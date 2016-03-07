e28644b45f88d17649882fc9bd91542a66e249d5
Marsian <mars@Marss-MacBook-Pro.local>
2016-03-06 21:55:55 -0800

    Lao Huang Li

--- App/Day/dayApp.js
+++ App/Day/dayApp.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var request = require('request');
+var lunarCalendar = require('lunar-calendar');
 
 var app = module.exports = express();
 
@@ -16,6 +17,7 @@ app.use(methodOverride());
 // page cache
 var cache = { 
     'index.html': fs.readFileSync('App/Day/index.html'),
+    'huangli': JSON.parse(fs.readFileSync('Assets/Data/huangli.json'))
 };
 
 // main route =================
@@ -51,6 +53,30 @@ app.post('/api/getCurrentLocation', function(req, res) {
             
             res.json({ results: data.results[0] }); 
         });            
-
     }
+
+});
+
+// get lunar calendar
+app.get('/api/getLunarCalendar', function(req, res) {
+    var date = new Date();
+    if (req.params.date) {
+        var date = new Date(req.params.date);
+    }
+
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    result = lunarCalendar.solarToLunar(year, month, day);
+    result.date = date;
+
+    var month_pad = ("0" + month).slice(-2);
+    var day_pad = ("0" + day).slice(-2);
+    var entry = year + '-' + month_pad + '-' + day_pad;
+    var huangli = cache.huangli.data[entry];
+    result.yi = huangli.yi;
+    result.ji = huangli.ji;
+
+    res.json(result); 
 });
