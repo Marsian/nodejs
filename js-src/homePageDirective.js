6a2154ef1991c6e904122beaeba393811f0c3cdc
Yanxi Wang <mars@Yanxis-MacBook-Pro.local>
2016-12-18 15:27:39 -0800

    Refactor homepage, first round

--- js-src/homePageDirective.js
+++ js-src/homePageDirective.js
@@ -1,7 +1,20 @@
 var app = angular.module('app');
 
-app.directive('homePage', function() {
+app.directive('homePage', function($window) {
     return {
-        templateUrl: 'Template/homePage.html'
+        templateUrl: 'Template/homePage.html',
+        link: function ($scope, $element) {
+            // scrool to previous position
+            $element.ready(function () {
+                if (sessionStorage.scrollTop != "undefined") {
+                    $(window).scrollTop(sessionStorage.scrollTop);
+                }
+            });
+
+            // save scroll position
+            angular.element($window).bind("scroll", function() {
+                sessionStorage.scrollTop = $(this).scrollTop();
+            });
+        }
     };
 });
--- js-src/indexController.js
+++ js-src/indexController.js
@@ -1,14 +1,31 @@
 var app = angular.module('app');
 
-app.controller('indexController', [ '$scope', function($scope) {
+app.controller('indexController', [ '$scope', '$window', function($scope, $window) {
+
+    $scope.showNavMenu = false;
+    $scope.toggleNavMenu = function (event) {
+        if (event) {
+            event.stopPropagation();
+            event.preventDefault();
+        }
+        $scope.showNavMenu = !$scope.showNavMenu;
+    };
+    $scope.closeNavMenu = function () {
+        if ($scope.showNavMenu)
+            $scope.showNavMenu = false;
+    };
+
+    $scope.scrollDownPage = function () {
+        var scrollHeight = $window.innerHeight + "px";
+        $("body").animate({ scrollTop: scrollHeight }, 500);
+    };
 
     $scope.currentTab = 'Home';
     $scope.selectedTabName = function() {
         return $scope.currentTab;
-    }
+    };
 
     $scope.selectTab = function( tab ) {
         $scope.currentTab = tab;
-    }
-
+    };
 }]);
